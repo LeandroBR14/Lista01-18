37a4ae092bc0f951be0dec10af65534c44dcc69d
Leandro Scalabrini <scalabrinileandro12@gmail.com>
2023-04-04 08:05:29 -0300

    Atualização 1

--- src/App.java
+++ src/App.java
@@ -1,25 +1,21 @@
 import java.util.Scanner;
-import java.lang.Math;
 public class App {
     public static void main(String[] args) {
-        double tamanho;
-        double velocidade;
-
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("Qual o tamanho do arquivo a ser baixado? ");
-        tamanho = teclado.nextDouble();
+        double tamanho = teclado.nextDouble();
 
         System.out.println("Qual a velocidade da sua internet? ");
-        velocidade = teclado.nextDouble();
+        double velocidade = teclado.nextDouble();
 
-        double tempoSeg = Math.floor((tamanho / velocidade));
-        double tempoMin = Math.floor((tempoSeg / 60));
-        double TempoMinRes = Math.floor((tempoSeg % 60));
+        double tempoSegundos = tamanho / velocidade;
+        int horas = (int) (tempoSegundos / 3600);
+        int minutos = (int) ((tempoSegundos % 3600) / 60);
+        int segundos = (int) (tempoSegundos % 60);
+        
+        System.out.printf("Você baixará esse arquivo em %d horas, %d minutos e %d segundos ", horas, minutos, segundos);
 
-        System.out.print("Você baixará esse arquivo em " +tempoMin);
-        System.out.print(" minutos e "+TempoMinRes);
-        System.out.println(" segundos.");
         teclado.close();
     }
 }
